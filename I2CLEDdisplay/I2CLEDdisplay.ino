cdbec8d52d7212d7cbbec8c96b988912b5f6be73
John W. Stokes, Jr. <tkdjohn71@gmail.com>
2023-11-03 02:09:45 -0400

    idk

--- I2CLEDdisplay/I2CLEDdisplay.ino
+++ I2CLEDdisplay/I2CLEDdisplay.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>  // wants teh LiquidCrystal I2C library
 
-LiquidCrystal_I2C lcd(0x3F, 16, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F, 40, 2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 bool firstPass = true;
 void setup() {
   lcd.init();  // initialize the lcd
@@ -11,39 +11,45 @@ void setup() {
 
 void loop() {
   if (firstPass) {
-    lcd.print("ABCDEFGHIJKLMNO\P");
+    lcd.print("ABCDEFGHIJKLMNOP");
+    lcd.setCursor(0,1);
     delay(100);
     lcd.print("QRSTUVWXYZ123456");
     firstPass = false;
   }
 
-  // when characters arrive over the serial port...
-  if (Serial.available()) {
-    // wait a bit for the entire message to arrive
-    delay(100);
-    // clear the screen
-    lcd.clear();
-    int charsDisplayed = 0;
-    int shifts = 0;
-    // read all the available characters
-    while (Serial.available() > 0) {
-      // display each character to the LCD
-      char buffer[65];
-      size_t size = Serial.readBytes(buffer, 64);
-      buffer[size] = '\0';
-      charsDisplayed += size;
-      Serial.print(buffer);
-      Serial.print("|");
-      lcd.print(buffer);
-      while (charsDisplayed > shifts + 16) {
-
-        lcd.scrollDisplayLeft();
-        shifts++;
-        delay(150);
-      }
-    }
-    Serial.println("");
-  }
+  
 }
 
-void writeToDisplay(char *)
+void serialEvent() {
+  int colsSent = 0;
+  int shifts = 0;
+
+  // read all the available characters
+  while (Serial.available()) {
+    lcd.home();
+    lcd.clear();
+  
+    char buffer[17];
+    size_t size1 = Serial.readBytes(buffer, 16);
+    buffer[size1] = '\0';
+    lcd.print(buffer);
+
+    size_t size2 = 0;
+    if (Serial.available()) {
+      lcd.setCursor(0,1);
+      size2 = Serial.readBytes(buffer, 16);
+      buffer[size2] = '\0';
+      lcd.print(buffer);
+    }
+
+    colsSent = max(size1, size2);
+    delay(250);
+    for (int shifts = 16; colsSent > shifts; shifts++) {
+      lcd.scrollDisplayLeft();
+      delay(200);
+    }  
+    delay(1000);
+    // Serial.println("one");
+  }
+}
